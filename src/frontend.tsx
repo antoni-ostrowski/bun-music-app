5eb46f9f7a7a8132e49ced69810a39e978eec298
antoni-ostrowski <antost360@gmail.com>
2025-08-24 14:57:10 +0200

    tanstack router

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -4,25 +4,52 @@
  *
  * It is included in `src/index.html`.
  */
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './app/App'
-import { MyTrpcProvider } from './app/trpc/provider'
-import './index.css'
-const elem = document.getElementById('root')!
-const app = (
-  <StrictMode>
-    <MyTrpcProvider>
-      <App />
-    </MyTrpcProvider>
-  </StrictMode>
-)
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { App } from './app/App'
+// import { MyTrpcProvider } from './app/trpc/provider'
+// import './index.css'
+// const elem = document.getElementById('root')!
+// const app = (
+//   <StrictMode>
+//     <MyTrpcProvider>
+//       <App />
+//     </MyTrpcProvider>
+//   </StrictMode>
+// )
 
-if (import.meta.hot) {
-  // With hot module reloading, `import.meta.hot.data` is persisted.
-  const root = (import.meta.hot.data.root ??= createRoot(elem))
-  root.render(app)
-} else {
-  // The hot module reloading API is not available in production.
-  createRoot(elem).render(app)
+// if (import.meta.hot) {
+//   // With hot module reloading, `import.meta.hot.data` is persisted.
+//   const root = (import.meta.hot.data.root ??= createRoot(elem))
+//   root.render(app)
+// } else {
+//   // The hot module reloading API is not available in production.
+//   createRoot(elem).render(app)
+// }
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Import the generated route tree
+import { routeTree } from './app/routeTree.gen'
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+const rootElement = document.getElementById('root')!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  )
 }
